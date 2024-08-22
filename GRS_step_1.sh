378fb069d045f29e2d366aa1c4be3d58cf2ae66d
Harry Green <36624710+hdg204@users.noreply.github.com>
2024-08-22 10:01:10 +0100

    Update GRS_step_1.sh

--- GRS_step_1.sh
+++ GRS_step_1.sh
@@ -3,11 +3,7 @@
 curl https://www.chg.ox.ac.uk/~gav/resources/bgen_v1.1.4-Ubuntu16.04-x86_64.tgz > bgen.tgz
 curl https://www.chg.ox.ac.uk/~gav/resources/qctool_v2.0.8-CentOS_Linux7.6.1810-x86_64.tgz > qctool.tgz
 curl https://s3.amazonaws.com/plink1-assets/plink_linux_x86_64_20231211.zip > plink.zip
-
-
-curl -s https://s3.amazonaws.com/plink2-assets/ > plinkweb
-grep -oP 'plink2_linux_x86_64_\d{8}.zip' plinkweb | sort | tail -n 1 > plinkfile
-curl -o plink2.zip $(paste -d "" <(echo -n "https://s3.amazonaws.com/plink2-assets/") plinkfile)
+curl https://raw.githubusercontent.com/hdg204/GRS-Nexus/main/plink2_linux_x86_64_20240818.zip > plink2.zip
 
 tar -xvzf bgen.tgz
 tar -xvzf qctool.tgz
